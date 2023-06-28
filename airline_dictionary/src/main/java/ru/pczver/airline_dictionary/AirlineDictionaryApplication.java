577ba1787e4cf5100f727d3abf9915e5daa5f912
Kosenkov Ivan <bouncer77rus@gmail.com>
2023-06-28 12:38:30 +0300

    init

---
+++ airline_dictionary/src/main/java/ru/pczver/airline_dictionary/AirlineDictionaryApplication.java
@@ -0,0 +1,13 @@
+package ru.pczver.airline_dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineDictionaryApplication.class, args);
+	}
+
+}
---
+++ airline_dictionary/src/test/java/ru/pczver/airline_dictionary/AirlineDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pczver.airline_dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
